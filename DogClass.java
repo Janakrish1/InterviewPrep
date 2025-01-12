dddb6979a4841e02bea42f4510879137179061b2
Jana <janakrish3011@gmail.com>
2025-01-11 23:38:30 -0500

    brushing up the oops concepts

---
+++ DogClass.java
@@ -0,0 +1,39 @@
+public class Dog {
+    String breed;
+    String color;
+    int age;
+    String name;
+
+    public Dog(String name) {
+        this.name = name;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void printDetails() {
+        System.out.println(this.breed);
+        System.out.println(this.color);
+        System.out.println(this.age);
+    }
+}
+
+public class Main {
+    public static void main(String args[]) {
+        Dog dog = new Dog("Tommy");
+
+        dog.setBreed("Golden Retriever");
+        dog.setColor("Gold");
+        dog.setAge(12);
+        dog.printDetails();
+    } 
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # InterviewPrep
+
+Classes 
+    user defined blueprint which can have multiple objects for same class. This binds the data and the methods 
+Objects 
+    basic unit of the object oriented programming and it is the instance of the class. These methods and data members are accessed through the objects
+
+To make class attributed read only declare the variable with final keyword
+final String name = "Jana";
+
